30871370d8544e86135aa51bef96469fdf5b85b0
Ajayi <ajayidavies7@gmail.com>
2022-11-16 15:57:02 -0800

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""
-Using https://jsonplaceholder.typicode.com
-returns info about employee TODO progress
-Implemented using recursion
-"""
-import re
-import requests
-import sys
-
-
-API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
+'''Retrieves data using a RESTful API'''
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API, id)).json()
-            todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    import requests
+    import sys
+
+    id = sys.argv[1]
+    site = 'https://jsonplaceholder.typicode.com'
+    user_url = site + '/users/' + id
+    task_done_url = site + '/todos?userId=' + id + '&completed=true'
+    task_not_done_url = site + '/todos?userId=' + id + '&completed=false'
+
+    user = requests.get(user_url)
+    todos = requests.get(task_done_url)
+    todos_undone = requests.get(task_not_done_url)
+    task_done = len(todos.json())
+    task_not_done = len(todos_undone.json())
+    task_tot = task_done + task_not_done
+    name = user.json().get('name')
+
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          task_done,
+                                                          task_tot))
+    for i in todos.json():
+        print('\t ' + i.get('title'))
