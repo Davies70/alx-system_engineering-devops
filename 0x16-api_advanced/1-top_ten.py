302bf78b19b8f7f2f6c64091fea27178b3ae7c06
Ajayi Davies <ajayidavies7@gmail.com>
2022-11-30 22:29:01 +0100

    task two done with recursion

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,29 +5,28 @@ of the first 10 hot posts listed for a given subreddit
 """
 import requests
 
+
 def json_find(d, key):
+    """ prints to 10 hot subreddits """
     if isinstance(d, dict):
         if key in d.keys():
-            return d.get(key)
-        return "".join([ json_find(d[k],key) for k in d.keys() ])
+            print(d.get(key))
+        [json_find(d[k], key) for k in d.keys()]
+
     elif isinstance(d, list):
-        return "".join([ json_find(x,key) for x in d ])
+        for item in d:
+            json_find(item, key)
     else:
-        return ""
+        pass
 
 
 def top_ten(subreddit):
     """ return top ten hot posts"""
-
     if subreddit is None or type(subreddit) is not str:
         print(None)
 
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    hot_lists = requests.get(url,
-                            headers={'User-Agent':
-                            '0x16-api_advanced:project:\
-                            v1.0.0 (by /u/lordzeus9000)'}).json()
-    if 'title' in hot_lists.keys():
-        return hot_lists.get('title')
-    for key in hot_lists.keys():
-        print(json_find(hot_lists[key], 'title'))
+    headers = {'User-Agent': '0x16-api_advanced: project: v1.0.0 \
+               (by /u/lordzeus9000)'}
+    hot_lists = requests.get(url, headers=headers).json()
+    json_find(hot_lists, 'title')
